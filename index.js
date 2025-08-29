9c7802bb3e744e141d6f06f36aef3a395e072d35
zohre-1982 <z82.siahpoush@gmail.com>
2025-08-29 13:54:04 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// const age =30;
+// const Age =20;
+// const myName = "siahpoush";
+// const scolarShip = true;
+// let age2 =20;
+// console.log(age);
+alert("hello")
+const userinfo ={name:"siahpoush" , number : 547689 , result : true};
+console.log(typeof userinfo.result);
+
+const soundengeering = ["akbari","802314", "3 to 4", "true"];
+console.log(soundengeering);
+
+function sound(name , number ,result) {console.log("sound" +" " +name +" " +result);}
+sound("akbari", "802314", "true");
+
